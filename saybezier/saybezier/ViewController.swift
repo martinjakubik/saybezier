902b14a813bd6ffe85b684cff086fc76bb882a4e
martin jakubik <martin.jakubik@gmail.com>
2019-02-20 22:21:51 +0100

    removed gesturerecognizers

--- saybezier/saybezier/ViewController.swift
+++ saybezier/saybezier/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
 
     var gestureRecognizers:[UIGestureRecognizer] = []
 
+    enum TapState {
+
+        case touchUp, firstTouchDown, firstTouchUp, secondTouchDown
+
+    }
+
     enum PathState {
 
         case noPath, startPoint, endPath
@@ -31,6 +37,16 @@ class ViewController: UIViewController {
         }
     }
 
+    enum DotSize {
+
+        case small, big
+
+    }
+
+    var tapState:TapState = .touchUp
+    var firstTouchTime:Date = Date()
+    let doubleTapInterval:TimeInterval = TimeInterval(floatLiteral: 0.8)
+    
     let spotRadius:CGFloat = 25.0
 
     var paths:[UIBezierPath] = []
@@ -69,20 +85,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view = SKView()
 
-        let singlePressRecognizer = TouchDownGestureRecognizer(target: self, action: #selector(self.handleSinglePress(_:)))
-        singlePressRecognizer.numberOfTapsRequired = 1
-        self.gestureRecognizers.append(singlePressRecognizer)
-        self.view.addGestureRecognizer(singlePressRecognizer)
-
-        let singlePanRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(_:)))
-        self.gestureRecognizers.append(singlePanRecognizer)
-        self.view.addGestureRecognizer(singlePanRecognizer)
-
-        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleDoubleTap(_:)))
-        doubleTapRecognizer.numberOfTapsRequired = 2
-        self.gestureRecognizers.append(doubleTapRecognizer)
-        self.view.addGestureRecognizer(doubleTapRecognizer)
-
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -96,7 +98,7 @@ class ViewController: UIViewController {
         self.skView.showsNodeCount = true
         self.skView.ignoresSiblingOrder = true
 
-        os_log("found scene with size: %f x %f", log:self.log, type:.debug, scene.size.width, scene.size.height)
+        os_log("found scene with size: %f x %f at anchor point x: %f, y: %f", log:self.log, type:.debug, scene.size.width, scene.size.height, scene.anchorPoint.x, scene.anchorPoint.y)
         self.backgroundNode.size = self.scene.size
         self.backgroundNode.position = CGPoint(
             x: self.scene.size.width / 2,
@@ -106,17 +108,91 @@ class ViewController: UIViewController {
         self.scene.addChild(backgroundNode)
         self.skView.presentScene(scene)
 
+        makeRuler()
+
     }
 
-    @objc func handleSinglePress(_ sender: UIGestureRecognizer) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        let locationInView = sender.location(in: self.view)
-        let locationInScene = convertToPointInScene(from: locationInView)
+        if (touches.count == 1) {
+
+            switch (self.tapState) {
+            case .touchUp:
+
+                self.tapState = .firstTouchDown
+                self.firstTouchTime = Date()
+
+                // starts timer and calls handleSingleTouchDown after double-tap timer expires
+                if let touch = touches.first {
+
+                    let locationInScene = touch.location(in: self.scene)
+                    handleSingleTouchDown(locationInScene: locationInScene)
+
+                }
+                break
+
+            case .firstTouchDown:
+
+                // can't happen
+                break
+
+            case .firstTouchUp:
+
+                let now = Date()
+                if (now <= self.firstTouchTime + doubleTapInterval) {
+
+                    self.tapState = .secondTouchDown
+
+                }
+                break
+
+            case .secondTouchDown:
+
+                // can't happen
+                break
+
+            }
+
+        }
+
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        if (touches.count == 1) {
+
+            if let touch = touches.first {
+
+                let locationInScene = touch.location(in: self.scene)
+                handleSingleTouchMoved(locationInScene: locationInScene)
+
+            }
+
+        }
+
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        if (touches.count == 1) {
+
+            if let touch = touches.first {
+
+                let locationInScene = touch.location(in: self.scene)
+                handleSingleTouchUp(locationInScene: locationInScene)
+
+            }
+
+        }
+
+    }
+
+    func handleSingleTouchDown(locationInScene: CGPoint) {
 
         switch self.pathState {
         case .noPath:
 
-            os_log("starting path at (scene) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
+            os_log("single touch down - starting path at (scene) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
 
             let spot = UIBezierPath(arcCenter: locationInScene, radius: spotRadius, startAngle: 0.0, endAngle: CGFloat(2.0 * Double.pi), clockwise: true)
             self.currentSpotNode = makeSpotNode(spot: spot)
@@ -141,11 +217,11 @@ class ViewController: UIViewController {
 
     }
 
-    @objc func handleDoubleTap(_ sender: UIGestureRecognizer) {
+    func handleDoubleTap(locationInScene: CGPoint) {
 
         if self.pathState == .noPath {
 
-            os_log("clearing scene", log:self.log, type:.debug)
+            os_log("double tap - clearing scene", log:self.log, type:.debug)
             os_log("------------------------", log:self.log, type:.debug)
             self.backgroundNode.removeAllChildren()
             self.pathState = .noPath
@@ -155,58 +231,7 @@ class ViewController: UIViewController {
 
     }
     
-    @objc func handlePan(_ sender: UIPanGestureRecognizer) {
-
-        let state = sender.state
-        
-        switch state {
-        case .began:
-            self.handlePanBegan(sender: sender)
-        case .changed:
-            self.handlePanChanged(sender: sender)
-        default:
-            os_log("doing nothing", log:self.log, type:.debug)
-        }
-
-    }
-
-    func handlePanBegan(sender: UIGestureRecognizer) {
-
-        let locationInView = sender.location(in: self.view)
-        let locationInScene = convertToPointInScene(from: locationInView)
-
-        switch self.pathState {
-        case .noPath:
-
-            os_log("starting path at (scene) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
-
-            let spot = UIBezierPath(arcCenter: locationInScene, radius: spotRadius, startAngle: 0.0, endAngle: CGFloat(2.0 * Double.pi), clockwise: true)
-            self.currentSpotNode = makeSpotNode(spot: spot)
-
-            if let spotNode = self.currentSpotNode {
-
-                self.backgroundNode.addChild(spotNode)
-
-            }
-
-            break
-
-        case .startPoint:
-
-            break
-
-        case .endPath:
-
-            break
-
-        }
-
-    }
-
-    func handlePanChanged(sender: UIGestureRecognizer) {
-
-        let locationInView = sender.location(in: self.view)
-        let locationInScene = convertToPointInScene(from: locationInView)
+    func handleSingleTouchMoved(locationInScene: CGPoint) {
 
         switch self.pathState {
         case .noPath:
@@ -221,7 +246,7 @@ class ViewController: UIViewController {
 
             let spotNodePosition = CGPoint(x: locationInScene.x - spotRadius, y: locationInScene.y)
 
-            os_log("moving path start from (scene) x: %f, y: %f to x: %f, y: %f", log:self.log, type:.debug, currentPoint.x, currentPoint.y, spotNodePosition.x, spotNodePosition.y)
+            os_log("pan changed - moving path start from (scene) x: %f, y: %f to x: %f, y: %f", log:self.log, type:.debug, currentPoint.x, currentPoint.y, spotNodePosition.x, spotNodePosition.y)
 
             self.currentSpotNode?.position = spotNodePosition
 
@@ -239,56 +264,50 @@ class ViewController: UIViewController {
 
     }
 
-    func handleTouchesEnded(_ touches: Set<UITouch>) {
+    func handleSingleTouchUp(locationInScene: CGPoint) {
 
-        for touch in touches {
+        switch self.pathState {
+        case .noPath:
 
-            let locationInScene = touch.location(in: self.scene)
+            os_log("dropping start path at (scene) x: %f, y: %f (view) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
 
-            switch self.pathState {
-            case .noPath:
+            let path:UIBezierPath = UIBezierPath()
+            path.move(to: locationInScene)
+            self.paths.append(path)
+            let pathNode = makePathNode(path: path)
+            self.backgroundNode.addChild(pathNode)
 
-                os_log("dropping start path at (scene) x: %f, y: %f (view) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
+            self.pathState = .startPoint
+            break
 
-                let path:UIBezierPath = UIBezierPath()
-                path.move(to: locationInScene)
-                self.paths.append(path)
-                let pathNode = makePathNode(path: path)
-                self.backgroundNode.addChild(pathNode)
+        case .startPoint:
 
-                self.pathState = .startPoint
-                break
+            os_log("adding point at (scene) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
 
-            case .startPoint:
+            let path = self.paths[self.paths.count - 1]
+            path.addLine(to: locationInScene)
+            let pathNode = makePathNode(path: path)
 
-                os_log("adding point at (scene) x: %f, y: %f", log:self.log, type:.debug, locationInScene.x, locationInScene.y)
+            let spot:UIBezierPath = UIBezierPath(arcCenter: locationInScene, radius: 25, startAngle: 0.0, endAngle: CGFloat(2.0 * Double.pi), clockwise: true)
+            let shapeNode = makeSpotNode(spot: spot)
 
-                let path = self.paths[self.paths.count - 1]
-                path.addLine(to: locationInScene)
-                let pathNode = makePathNode(path: path)
+            self.backgroundNode.addChild(shapeNode)
+            self.backgroundNode.addChild(pathNode)
 
-                let spot:UIBezierPath = UIBezierPath(arcCenter: locationInScene, radius: 25, startAngle: 0.0, endAngle: CGFloat(2.0 * Double.pi), clockwise: true)
-                let shapeNode = makeSpotNode(spot: spot)
+            self.pathState = .endPath
+            break
 
-                self.backgroundNode.addChild(shapeNode)
-                self.backgroundNode.addChild(pathNode)
+        case .endPath:
 
-                self.pathState = .endPath
-                break
+            let path = self.paths[self.paths.count - 1]
+            path.close()
 
-            case .endPath:
-
-                let path = self.paths[self.paths.count - 1]
-                path.close()
-
-                self.pathState = .noPath
-                break
-
-            }
-
-            os_log("state: %s", log:self.log, type:.debug, self.pathState.description())
+            self.pathState = .noPath
+            break
 
         }
+
+        os_log("state: %s", log:self.log, type:.debug, self.pathState.description())
 
     }
 
@@ -327,5 +346,72 @@ class ViewController: UIViewController {
 
         return pointInScene
     }
+
+    func makeRuler() {
+
+        let sceneWidth = Int(self.scene.size.width)
+        let sceneHeight = Int(self.scene.size.height)
+        let margin = CGFloat(40)
+
+        for x in 0 ... sceneWidth {
+
+            if (x % 50 == 0) {
+
+                let dot = makeDotNode(at: CGPoint(x: CGFloat(x), y: margin), dotSize: .big)
+                self.scene.addChild(dot)
+
+            } else if (x % 10 == 0) {
+
+                let dot = makeDotNode(at: CGPoint(x: CGFloat(x), y: margin), dotSize: .small)
+                self.scene.addChild(dot)
+
+            }
+
+        }
+
+        for y in 0 ... sceneHeight {
+
+            if (y % 50 == 0) {
+
+                let dot = makeDotNode(at: CGPoint(x: margin, y: CGFloat(y)), dotSize: .big)
+                self.scene.addChild(dot)
+
+            } else if (y % 10 == 0) {
+
+                let dot = makeDotNode(at: CGPoint(x: margin, y: CGFloat(y)), dotSize: .small)
+                self.scene.addChild(dot)
+
+            }
+
+        }
+
+    }
+
+    func makeDotNode(at position:CGPoint, dotSize:DotSize) -> SKShapeNode {
+
+        var radius = CGFloat(2)
+        let strokeColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        var fillColor = UIColor.white
+        var zPosition = self.scene.zPosition + 2
+        
+        switch dotSize {
+        case .small:
+            break
+        case .big:
+            radius = 10
+            fillColor = UIColor.green
+            zPosition = self.scene.zPosition + 1
+            break
+        }
+        
+        let dot = SKShapeNode(circleOfRadius: radius)
+        dot.position = position
+        dot.strokeColor = strokeColor
+        dot.fillColor = fillColor
+        dot.zPosition = zPosition
+        return dot
+
+    }
+
 }
 
